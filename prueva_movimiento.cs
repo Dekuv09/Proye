df633b3ab7ebe12b9d17e561c942e04d30e8b524
Dekuv09 <131810724+Dekuv09@users.noreply.github.com>
2023-06-15 22:21:13 -0500

    Add files via upload
    Este es el código actualizado

--- prueva_movimiento.cs
+++ prueva_movimiento.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class prueva_movimiento : MonoBehaviour 
 {
+    //Declaración de variables
     public float fuerzaSalto;
     public int saltomulti;
     public LayerMask capaSuelo;
-    private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
     private bool mirandoDerecha;
     private int saltorestan;
     private Animator animator;
 
-    //1. Declaración de variables
     [Range(1, 10)] public float velocidad; //Velocidad del jugador
     Rigidbody2D rb2d;
     SpriteRenderer spRd;
@@ -21,7 +20,7 @@ public class prueva_movimiento : MonoBehaviour
     private void Start () 
     {
 
-        //2. Capturo y asocio los componentes Rigidbody2D y Sprite Renderer del Jugador
+        //Capturo y asocio los componentes Rigidbody2D y Sprite Renderer del Jugador
         rb2d = GetComponent<Rigidbody2D>();
         spRd = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent <BoxCollider2D>();
@@ -37,11 +36,11 @@ public class prueva_movimiento : MonoBehaviour
     void FixedUpdate () 
     {
         
-        //3. Movimiento horizontal
+        // Movimiento horizontal
         float movimientoH = Input.GetAxisRaw("Horizontal");
         rb2d.velocity = new Vector2(movimientoH * velocidad, rb2d.velocity.y);
 
-        //4. Sentido horizontal (para girar el render del jugador)
+        // Sentido horizontal (para girar el render del jugador)
         if (movimientoH > 0)
         {
             spRd.flipX = false;
@@ -81,6 +80,15 @@ public class prueva_movimiento : MonoBehaviour
             saltorestan = saltorestan - 1;
             rb2d.AddForce(Vector2.up*fuerzaSalto, ForceMode2D.Impulse);
 
+        }
+        
+        if ( Input.GetKeyDown(KeyCode.Space) || EstaEnSuelo()== false)
+        {
+            animator.SetBool("Skipping", true);
+        }
+        else
+        {
+            animator.SetBool("Skipping", false);
         }
 
     }
